d80487809da9be5b8d2577824c9857d3c4f64244
Gil Poiares-Oliveira <gil@poiares-oliveira.com>
2023-10-18 21:21:12 +0100

    Document creation system

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,16 +11,17 @@ export default defineConfig({
 				replacesTitle: true,
 			},
 			editLink: {
-				baseUrl: 'https://github.com/BioData-pt/wiki/edit/main/docs/',
+				baseUrl: 'https://github.com/BioData-pt/wiki/edit/main/',
 			},
 			social: {
 				github: 'https://github.com/BioData-PT',
 			},
 			sidebar: [
-				{label: 'Home', link:'/'},
+				{ label: 'Home', link: '/' },
+				{ label: 'New page', link: 'https://github.com/BioData-pt/wiki/new/main/src/content/docs?filename=CHANGE-ME.md&value=---%0Atitle%3A+Type+title+here%0Adescription%3A+Insert+short+description+here%0A---%0A%0AInsert+wiki+content+here+and+do+not+forget+to+change+the+name+of+the+file' },
 				{
 					label: 'Internal Systems',
-					autogenerate: {directory: 'internal-systems'},
+					autogenerate: { directory: 'internal-systems' },
 				},
 			],
 		}),
--- src/content/docs/index.md
+++
@@ -1,8 +0,0 @@
----
-title: Welcome
-description: Welcome to the BioData.pt Wiki
----
-
-## Welcome to the BioData.pt Wiki
-
-This collaborative wiki exists to help BioData.pt staff share knowledge!
